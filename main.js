cdce8814ed0126d7abd171a43e0e81c00bf34676
monmokk <myeongjikang910814@gmail.com>
2022-07-10 21:44:45 +0900

    ENH - cheerio, slug, ...

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import { count, increment } from './counter.js'
+import * as a from './a.js'
+import * as b from './b.js'
+
+console.log('a ->', a)
+console.log('b ->', b)
+
 
 console.log(count);
 increment();
 console.log(count);
-count++;
+// count++;
